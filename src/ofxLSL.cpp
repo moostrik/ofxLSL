66444ee5cf0fbdafd0fa146c8a978ef425bcc63d
Matthias Oostrik <m.oostrik@gmail.com>
2019-03-04 09:18:46 +0100

    tabs

--- src/ofxLSL.cpp
+++ src/ofxLSL.cpp
@@ -3,36 +3,36 @@
 ofxLSL::ofxLSL() : active(false), mappingUpdated(false) {}
 
 bool ofxLSL::start() {
-  if(active) return false;
-
-  thread = std::make_unique<std::thread>(&ofxLSL::update, this);
-  active = true;
-  return true;
+	if(active) return false;
+	
+	thread = std::make_unique<std::thread>(&ofxLSL::update, this);
+	active = true;
+	return true;
 }
 
 bool ofxLSL::stop() {
-  if(!active) return false;
-
-  active = false;
-  thread->join();
-  thread = nullptr;
-  return true;
+	if(!active) return false;
+	
+	active = false;
+	thread->join();
+	thread = nullptr;
+	return true;
 }
 
 void ofxLSL::update() {
-  while(active) {
-    try {
-		if(inlet == nullptr) {
-		  connect();
-		} else {
-		  pull();
+	while(active) {
+		try {
+			if(inlet == nullptr) {
+				connect();
+			} else {
+				pull();
+			}
+		} catch(lsl::timeout_error& e) {
+			disconnect();
+		} catch(lsl::lost_error& e) {
+			disconnect();
 		}
-	} catch(lsl::timeout_error& e) {
-		disconnect();
-	} catch(lsl::lost_error& e) {
-		disconnect();
 	}
-  }
 }
 
 void ofxLSL::disconnect() {
@@ -46,55 +46,56 @@ void ofxLSL::disconnect() {
 }
 
 void ofxLSL::connect() {
-//	std::vector<lsl::stream_info> resolvedStreams = lsl::resolve_streams(1.0);
-//
-//	for (auto &s : resolvedStreams)
-//		cout << s.source_id() << " " << s.name() + " (" + s.hostname() + ")" << endl;
+	//	std::vector<lsl::stream_info> resolvedStreams = lsl::resolve_streams(1.0);
+	//
+	//	for (auto &s : resolvedStreams)
+	//		cout << s.source_id() << " " << s.name() + " (" + s.hostname() + ")" << endl;
 	
-  auto streams = lsl::resolve_stream("desc/correlation", "R", 1, 2.f);
-  if(streams.size() == 0) return;
-
-  std::lock_guard<std::mutex> lock(mutex);
-  inlet = std::make_unique<lsl::stream_inlet>(streams.front(), 360, 0, false);
+	auto streams = lsl::resolve_stream("desc/correlation", "R", 1, 2.f);
+	if(streams.size() == 0) return;
 	
-  auto info = inlet->info(1.0f);
-
-  ofLogNotice() << "Connecting to " << info.name() << " at " << info.nominal_srate() << "hz";
+	std::lock_guard<std::mutex> lock(mutex);
+	inlet = std::make_unique<lsl::stream_inlet>(streams.front(), 360, 0, false);
+	
+	auto info = inlet->info(1.0f);
+	
+	ofLogNotice() << "Connecting to " << info.name() << " at " << info.nominal_srate() << "hz";
 	buffer.reserve(250.0);
 	sample_buffer.reserve(info.channel_count());
-
-  mapping.clear();
-  auto mappingXML = info.desc().child("mappings");
-  for (auto m = mappingXML.first_child(); !m.empty(); m = m.next_sibling()) {
-    bool firstSet = false;
-    pair<string,string> mapElement;
-    for (auto ab = m.first_child(); !ab.empty(); ab = ab.next_sibling()) {
-      for (auto uID = ab.first_child(); !uID.empty(); uID = uID.next_sibling()) {
-        if (!firstSet) {
-          firstSet =true;
-          mapElement.first = uID.value();
-        } else {
-          mapElement.second = uID.value();
-          mapping.emplace_back(mapElement);
-  } } } }
-  mappingUpdated = true;
+	
+	mapping.clear();
+	auto mappingXML = info.desc().child("mappings");
+	for (auto m = mappingXML.first_child(); !m.empty(); m = m.next_sibling()) {
+		bool firstSet = false;
+		pair<string,string> mapElement;
+		for (auto ab = m.first_child(); !ab.empty(); ab = ab.next_sibling()) {
+			for (auto uID = ab.first_child(); !uID.empty(); uID = uID.next_sibling()) {
+				if (!firstSet) {
+					firstSet =true;
+					mapElement.first = uID.value();
+				} else {
+					mapElement.second = uID.value();
+					mapping.emplace_back(mapElement);
+				} } } }
+	mappingUpdated = true;
 }
 
 void ofxLSL::pull() {
-    float ts = inlet->pull_sample(sample_buffer, 1.0);
-    if(ts) {
-      ofLogVerbose() << "Received sample";
-
-      ofxLSLSample sample;
-      sample.timestamp = ts;
-	  sample.sample = std::vector<float>(sample_buffer.begin(), sample_buffer.end());
-
-	  std::lock_guard<std::mutex> lock(mutex);
+	float ts = inlet->pull_sample(sample_buffer, 1.0);
+	if(ts) {
+		ofLogVerbose() << "Received sample";
+		
+		ofxLSLSample sample;
+		sample.timestamp = ts;
+		sample.sample = std::vector<float>(sample_buffer.begin(), sample_buffer.end());
+		
+		std::lock_guard<std::mutex> lock(mutex);
 		while(buffer.size() && buffer.size() >= buffer.capacity()) {
 			ofLogWarning() << "Buffer capacity reached";
 			buffer.erase(buffer.begin());
 		}
 		
-	  buffer.push_back(sample);
-    }
+		buffer.push_back(sample);
+	}
 }
+
--- src/ofxLSL.h
+++ src/ofxLSL.h
@@ -5,57 +5,50 @@
 #include "lsl_cpp.h"
 
 struct ofxLSLSample {
-  float timestamp = 0.0;
-  std::vector<float> sample;
+	float timestamp = 0.0;
+	std::vector<float> sample;
 };
 
 class ofxLSL : public ofThread {
-  
+	
 public:
 	ofxLSL();
 	~ofxLSL() { stop(); };
-  
-  bool start();
-  bool stop();
-  bool isConnected() {
-	  std::lock_guard<std::mutex> lock(mutex);
-	  return inlet != nullptr;
 	
-  }
-  
-  std::vector<ofxLSLSample> flush() {
-    std::lock_guard<std::mutex> lock(mutex);
-    auto currentBuffer = std::vector<ofxLSLSample>(buffer.begin(), buffer.end());
-	buffer.clear();
-	return currentBuffer;
-  };
+	bool start();
+	bool stop();
+	bool isConnected() {
+		std::lock_guard<std::mutex> lock(mutex);
+		return inlet != nullptr;
+		
+	}
 	
-  bool getMappingUpdated() {
-    if (mappingUpdated) {
-      mappingUpdated = false;
-      return true;
-    }
-    return false;
-  }
+	std::vector<ofxLSLSample> flush() {
+		std::lock_guard<std::mutex> lock(mutex);
+		auto currentBuffer = std::vector<ofxLSLSample>(buffer.begin(), buffer.end());
+		buffer.clear();
+		return currentBuffer;
+	};
 	
-  std::vector< pair<string, string> > getMapping() {
-    std::lock_guard<std::mutex> lock(mutex);
-    return mapping;
-  };
-  
+	std::vector< pair<string, string> > getMapping() {
+		std::lock_guard<std::mutex> lock(mutex);
+		return mapping;
+	};
+	
 private:
 	
-  void update();
-  void connect();
-  void disconnect();
-  void pull();
-  bool active;
-  std::vector<float> sample_buffer;
-  std::vector< pair<string, string> > mapping;
-  atomic<bool> mappingUpdated;
-  
-  std::mutex mutex;
-  std::unique_ptr<std::thread> thread;
-  std::unique_ptr<lsl::stream_inlet> inlet;
-  std::vector<ofxLSLSample> buffer;
+	void update();
+	void connect();
+	void disconnect();
+	void pull();
+	bool active;
+	std::vector<float> sample_buffer;
+	std::vector< pair<string, string> > mapping;
+	atomic<bool> mappingUpdated;
+	
+	std::mutex mutex;
+	std::unique_ptr<std::thread> thread;
+	std::unique_ptr<lsl::stream_inlet> inlet;
+	std::vector<ofxLSLSample> buffer;
 };
+
