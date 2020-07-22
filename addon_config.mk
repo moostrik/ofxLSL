513cd01371918744ef9091cd35eadcb3db7a61a9
M Oostrik <m.oostrik@gmail.com>
2020-07-22 12:20:58 +0200

    linux update

---
+++ addon_config.mk
@@ -0,0 +1,16 @@
+meta:
+    ADDON_NAME = ofxLSL
+    ADDON_DESCRIPTION = Interface for Lab Streaming Layer
+    ADDON_AUTHOR = Jean Jacques Warmerdam & Matthias Oostrik
+    ADDON_TAGS =
+    ADDON_URL = http://github.com/moostrik/ofxLSL
+
+common:
+    ADDON_INCLUDES = libs/labstreaminglayer/include
+    ADDON_INCLUDES += src
+    ADDON_DEPENDENCIES = ofxXmlSettings
+    ADDON_CPPFLAGS = -D_x64
+
+linux64:
+    ADDON_LIBS = libs/labstreaminglayer/lib/linux64/liblsl64.so
+    ADDON_CPPFLAGS += -D_LINUX
