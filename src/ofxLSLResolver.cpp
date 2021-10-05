7ed933e2c805429e2e105459c6728514707d4800
m.oostrik <matthias@oostrik.net>
2021-10-05 11:04:39 +0200

    resolver

---
+++ src/ofxLSLResolver.cpp
@@ -0,0 +1,92 @@
+﻿#include "ofxLSLResolver.h"
+
+using namespace lsl;
+using namespace ofxLSL;
+
+
+Resolver::Resolver() {
+  active = true;
+
+  resolver = make_unique<continuous_resolver>();
+  connectThread = std::make_unique<std::thread>(&Resolver::connect, this);
+}
+
+Resolver::~Resolver() {
+  active = false;
+  disconnect();
+
+  connectThread->join();
+  connectThread = nullptr;
+  resolver = nullptr;
+}
+
+
+void Resolver::connect() {
+  while (active) {
+    std::vector<stream_info> resolved_infos = resolver->results();
+    // add new streams
+    for (auto& info : resolved_infos) {
+      bool inletFound = false;
+      for (auto& inlet : inlets) {
+        if (isEqual(info, inlet->info())) {
+          inletFound = true;
+        }
+      }
+
+      if (!inletFound) {
+        ofLogNotice("ofxLSLReceiver::connect")
+            << "open stream'" << info.name() << "' with type '" << info.type()
+            << " and source ID '" << info.source_id() << "'";
+        auto inlet  = std::make_shared<stream_inlet>(info);
+        inlet->open_stream();
+
+        std::lock_guard<std::mutex> lock(connectMutex);
+        ofNotifyEvent(onConnect, inlet, this);
+        inlets.emplace_back(std::move(inlet));
+      }
+    }
+
+    // remove disconnected streams
+    for (int i = inlets.size() - 1; i >= 0; i--) {
+      auto& inlet = inlets[i];
+      bool found = false;
+      const auto& iI = inlet->info();
+      for (auto& rI : resolved_infos) {
+        if (isEqual(iI, rI)) {
+          found = true;
+        }
+      }
+      if (!found) {
+        ofLogNotice("ofxLSLReceiver::connect")
+            << "lost stream '" << iI.name() << "' with type '" << iI.type()
+            << " and source ID '" << iI.source_id() << "'";
+        std::lock_guard<std::mutex> lock(connectMutex);
+
+        ofNotifyEvent(onDisconnect, inlet, this);
+        inlets.erase(inlets.begin() + i);
+      }
+    }
+
+    sleep(500);
+  }
+}
+
+void Resolver::disconnect() {
+  {
+    std::lock_guard<std::mutex> lock(connectMutex);
+    for (auto& inlet : inlets) {
+      ofNotifyEvent(onDisconnect, inlet, this);
+      inlet->close_stream();
+    }
+    inlets.clear();
+  }
+}
+
+
+bool Resolver::isEqual(stream_info _infoA, stream_info _infoB) {
+  if (_infoA.name() == _infoB.name() && _infoA.type() == _infoB.type() &&
+      _infoA.source_id() == _infoB.source_id()) {
+    return true;
+  }
+  return false;
+}
---
+++ src/ofxLSLResolver.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "lsl_cpp.h"
+#include "ofLog.h"
+#include "ofMain.h"
+
+using namespace lsl;
+
+namespace ofxLSL {
+
+class Resolver {
+public:
+  // Disable copy and move
+  Resolver(Resolver const&) = delete;             // Copy construct
+  Resolver(Resolver&&) = delete;                  // Move construct
+  Resolver& operator=(Resolver const&) = delete;  // Copy assign
+  Resolver& operator=(Resolver&&) = delete;       // Move assign
+
+  // Allow public destruction by the shared pointer
+  ~Resolver();
+
+  static std::shared_ptr<Resolver> getInstance() {
+    static std::weak_ptr<Resolver> _weakInstance;
+    if (auto existingPtr = _weakInstance.lock()) return existingPtr;
+    auto newPtr = std::shared_ptr<Resolver>(new Resolver());
+    _weakInstance = newPtr;
+    return newPtr;
+  }
+
+  ofEvent<const std::shared_ptr<lsl::stream_inlet>&> onConnect;
+  ofEvent<const std::shared_ptr<lsl::stream_inlet>&> onDisconnect;
+
+private:
+  Resolver();
+
+  void connect();
+  void disconnect();
+
+  bool isEqual(stream_info _infoA, stream_info _infoB);
+
+  atomic<bool> active;
+  std::mutex connectMutex;
+  std::unique_ptr<std::thread> connectThread;
+
+  std::unique_ptr<continuous_resolver> resolver;
+  std::vector<std::shared_ptr<lsl::stream_inlet>> inlets;
+};
+}
